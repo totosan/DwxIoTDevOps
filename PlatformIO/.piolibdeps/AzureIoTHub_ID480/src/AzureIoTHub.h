151af8e14be04706e95297500a56cc48613e09d1
Tomow <ttomow@alegri.eu>
2019-09-14 11:16:31 +0200

    added pios files/folders

---
+++ PlatformIO/.piolibdeps/AzureIoTHub_ID480/src/AzureIoTHub.h
@@ -0,0 +1,18 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+#ifndef AZUREIOTHUB_H
+#define AZUREIOTHUB_H
+
+#include "AzureIoTUtility.h"
+
+#include "serializer.h"
+
+#include "iothub_client_ll.h"
+#include "iothub_message.h"
+#include "iothubtransporthttp.h"
+#include "iothubtransportmqtt.h"
+#include "iothub_client_options.h"
+
+#define AzureIoTHubVersion "1.0.46"
+#endif
